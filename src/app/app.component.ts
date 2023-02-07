3cdd2159cc2fa91cb0fe140ffbde0269df9eb1f6
Roosvelandiaaval <roosvell.velandia@avaldigitallabs.com>
2023-02-07 10:37:57 -0500

    property bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // when a variable is private i can't access it from html
   //private name = 'roosvell';
-  name = 'Roosvell';
+  name = 'Luffy';
   age = 55;
   img = 'https://i.ytimg.com/vi/ha0-qytMD9k/maxresdefault.jpg'
   btnDisabled = true;
