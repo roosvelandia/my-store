c13e4253b720df5720fcb2b8dbb07ab79a3e516d
Roosvelandiaaval <roosvell.velandia@avaldigitallabs.com>
2023-02-07 10:26:54 -0500

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,11 @@ export class AppComponent {
   name = 'Roosvell';
   age = 55;
   img = 'https://i.ytimg.com/vi/ha0-qytMD9k/maxresdefault.jpg'
+  btnDisabled = true;
+  person ={
+    name: 'Robin',
+    age: 19,
+    avatar: 'https://i.seadn.io/gae/HMXjc3fdO98oMW__BMaHRsE05c9IAiH0QepxAdmgi7df1b27yiKSQzq0STXReTmmNOjhL1LJZ-9DXpAUtJIxVBoYS4cFBqgkUTgj?auto=format&w=1000'
+
+  }
 }
